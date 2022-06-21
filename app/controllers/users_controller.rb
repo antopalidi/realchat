a4f6d89fc3187b15f1a1dd890ccad984126996b5
antopalidi <t.opalidisanna@gmail.com>
2022-06-21 18:12:06 +0200

    fix create_private_room in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
     @room_name = get_name(@user, current_user)
-    @single_room = Room.where(name: @room_name).first || Room.creatr_private_room([@user, @current_user], @room_name)
+    @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, @current_user], @room_name)
 
     @message = Message.new
     @messages = @single_room.messages.order(created_at: :asc)
