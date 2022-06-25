13982e19679ec22012a1f79f589eac8b5d743b8b
antopalidi <t.opalidisanna@gmail.com>
2022-06-25 15:27:19 +0200

    heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ module Realchat
   class Application < Rails::Application
     config.active_storage.variant_processor = :mini_magick
 
-    config.after_initialize do |_config|
-      User.update_all(status: User.statuses[:offline])
-    end
+    # config.after_initialize do |_config|
+    #   User.update_all(status: User.statuses[:offline])
+    # end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
