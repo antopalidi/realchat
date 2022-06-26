703c5b1dfed4c797356af957d34ab2e10423a62b
antopalidi <t.opalidisanna@gmail.com>
2022-06-26 08:54:54 +0200

    add aws

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'autoprefixer-rails'
 
 gem 'carrierwave'
 gem 'rmagick'
+gem 'fog-aws'
 
 gem 'dotenv-rails'
 
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,8 +3,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+    if Rails.env.production?
+      storage :fog
+    else
+      storage :file
+    end
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
