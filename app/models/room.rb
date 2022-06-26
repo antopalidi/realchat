af5b3bdc78464f8af528afb8c930ddbb73f62848
antopalidi <t.opalidisanna@gmail.com>
2022-06-26 13:50:49 +0200

    refactoring

--- app/models/room.rb
+++ app/models/room.rb
@@ -9,7 +9,7 @@ class Room < ApplicationRecord
   after_create_commit { broadcast_if_public }
 
   def broadcast_if_public
-    broadcast_append_to 'rooms'unless self.is_private
+    broadcast_append_to 'rooms'unless is_private
   end
 
   def self.create_private_room(users, room_name)
